c1bf2524b5927a360fae2d6715b38947b5c82eda
SamMrach <sammerche84@gmail.com>
2024-06-03 16:56:18 +0100

    set up mailgun smtp server

--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -1,11 +1,10 @@
 import type { NextAuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 import EmailProvider from "next-auth/providers/email";
-import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "./prisma";
 
 export const authOptions = {
-  adapter: PrismaAdapter(prisma),
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID as string,
@@ -23,4 +22,6 @@ export const authOptions = {
       from: process.env.EMAIL_FROM,
     }),
   ],
+  adapter: PrismaAdapter(prisma),
+
 } satisfies NextAuthOptions;
